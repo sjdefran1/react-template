c5ddaca6ad14b22da78c8070820fc455595c1330
Sam DeFrancisco <sjdefran@gmail.com>
2022-10-07 01:43:32 -0500

    more work on abt, added picture, trying to add tech stack

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,16 @@
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import Chip from '@material-ui/core/Chip'
+import Grid from '@material-ui/core/Grid'
 import Avatar from '@material-ui/core/Avatar'
 import { about } from '../../portfolio'
 import './About.css'
 import resume from '../../static/DeFrancisco_ResumeF22.pdf'
 import avatar from '../../static/isu_logo.PNG'
+import mePicture from '../../static/pics/steps-bw.jpg'
+import pySVG from '../../static/pics/python-5.svg'
+
+//      <img src={mePicture} alt='me' style={{zIndex:0}} /> 
 
 
 const About = () => {
@@ -13,20 +18,37 @@ const About = () => {
   
 
   return (
-    <div className='about center'>
-      {name && (
-        <h1>
-          Hi, I am <span className='about__name'>{name}.</span>
-        </h1>
-      )}
+    
+    <div className='about center'> 
+      <Grid container spacing={3} >
+        <Grid item xs={12} sm={6}>
+          {name && (
+            <h1 className='about__role'>
+              Hi, I am <span className='about__name'>{name}.</span>
+            </h1>
+          )}
+          {role && <h2 className='about__role'>A {role}.</h2>}
+          <Chip
+            color = 'primary'
+            label="Junior"
+          />
+          <Grid item xs={6} sm = {3}>
+            <Avatar alt="Remy Sharp" src={pySVG}/>
+          </Grid>
+          <Grid item xs={6} sm = {3}>
+            <Avatar alt="Remy Sharp" src={pySVG}/>
+          </Grid>
+        </Grid>
 
-      {role && <h2 className='about__role'>A {role}.</h2>}
+        <Grid item xs={12} sm={6}>
+          <img className='me_header' src={mePicture} alt='me' width="750" height="500"/> 
+        </Grid>
+        
+      </Grid>
+
       
       
-      <Chip
-        color = 'primary'
-        label="Junior"
-      />
+      
       
 
       <div className='about__contact center'>
